b9715a5c2bbbc3cc74d2d45a71a40aa9132f52ef
Miriani80 <mirianipavliashvili995@gmail.com>
2024-07-26 21:07:04 +0400

    commit

---
+++ Day 04/Homework/task2.py
@@ -0,0 +1,21 @@
+"""2) მომხმარებელს input-ის გამოყენებით შემოატანინეთ სახელი, გვარი, ასაკი და ქალაქი. როცა  შემოიტანს
+მომხმარებელი ყველაფერს მერე უნდა დაუპრინტოთ გრძელი წინადადება"""
+
+name = input("enter you name ")
+surname = input("enter your surname ")
+age = input("enter your age ")
+city = input("enter your city ")
+print(name)
+print(surname)
+print(age)
+print(city)
+
+
+
+
+
+
+
+
+
+
---
+++ Day 07/Classwork/task1.py
@@ -0,0 +1,15 @@
+"""მომხმარებელს input-ის გამოყენებით უნდა შემოატანინოთ 2 რიცხვი და შმდეგ ამ 2 რიხვით უნდა გააკეთოთ მათემატიკური მაგალითები/ოპერაციები"""
+
+num1= int(input("enter your number1 "))
+num2= int(input("enter your number2 "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+
+
+
+
+
